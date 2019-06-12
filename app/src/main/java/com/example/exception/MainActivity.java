03be70207e0792e59d8b44a46f8a5086b553b60d
Arunda Victor <victoroyugi31@gmail.com>
2019-06-13 01:48:14 +0300

    add permissions

--- app/src/main/java/com/example/exception/MainActivity.java
+++ app/src/main/java/com/example/exception/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.exception;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import icepick.State;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
