fbf097d3a441cdf162689749fc4e1c13eaab092d
Arunda Victor <victoroyugi31@gmail.com>
2019-06-13 02:55:19 +0300

    refactor mainactivity

--- app/src/main/java/com/example/exception/MainActivity.java
+++ app/src/main/java/com/example/exception/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.exception;
 
+import android.content.Context;
+import android.support.multidex.MultiDexApplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 import icepick.State;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MultiDexApplication {
+    public static Context context;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private static MainActivity mInstance;
+
+    public void onCreate() {
+        super.onCreate();
+        this.context = this;
+        mInstance = this;
+
+
+        Fresco.initialize(this.context);
+
+
+
+
     }
+
+    public static synchronized MainActivity getInstance() {
+        return mInstance;
+    }
+
+    public static Context getAppContext() {
+        return MyApplication.context;
+    }
+
+
 }
+
